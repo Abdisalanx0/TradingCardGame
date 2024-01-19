c601f7087b9dc82d904baf729298a4fc2c112cf7
yuruse419 <chaseorion19@gmail.com>
2024-01-18 21:15:49 -0600

    created login page

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
+import { AuthProvider } from './contexts/AuthContext'
+import Login from './pages/Login'
 import './App.css'
 
 function App() {
   return (
-    <>
-      
-    </>
+    <AuthProvider>
+      <Login></Login>
+    </AuthProvider>
   )
 }
 
---
+++ src/components/footer/FooterComponent.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import '../../css/footer/FooterComponent.css'
+
+const FooterComponent = () => {
+  return (
+    <footer id='page-footer'>
+      <p>Created by Abdisalan Mohamed, Chase Shertzer, & Hamze Jama</p>
+    </footer>
+  )
+}
+
+export default FooterComponent
