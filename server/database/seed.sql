e67a2ad71068c284bd45fb154e179afa03047872
Abdisalanx0 <Abdisalanm250@gmail.com>
2024-02-05 01:56:16 -0600

    Added registration and password Hashing

--- server/database/seed.sql
+++ server/database/seed.sql
@@ -2,8 +2,9 @@
 
 INSERT INTO tcg_user (username, password_hash) VALUES (
   'admin',
-  null
+  "$2y$10$bDKXK7pxePpSKPzPU4doIOY2SMsXyLwjZBQrUnQXEy3by4dsVrB7m"
 );
+--password = 1234
 
 -- replace null with a password hash when you know what tech you'll be using to hash passwords
 -- also make a note at the top of this file like, "923#@$IYGUbkjhdjkdo&*& is the hash for 'fakepassword'" so we can reference it during development
